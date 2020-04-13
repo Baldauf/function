d84b64e3da628f354d8fa136aa6a94b9fd08a2dd
John-George Sample <Johngeorgesample@gmail.com>
2020-04-13 15:06:04 -0400

    start FormEmbed module

---
+++ src/components/Modules/FormEmbed/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    foo
+  </div>
+</template>
+
+<script>
+import BaseComponent from '~/src/components/Templates/BaseComponent'
+
+export default {
+  extends: BaseComponent,
+  props: {
+    name: {
+      type: String,
+      default: 'FormEmbed'
+    }
+  },
+  created () {
+    let hsScript = document.createElement('script')
+    hsScript.setAttribute('src',"//js.hsforms.net/forms/v2.js")
+    document.head.appendChild(hsScript)
+  },
+  mounted () {
+    console.log('hit')
+    hbspt.forms.create({
+            portalId: "7156206",
+            formId: "d5dd2eec-b878-4b06-aa0d-81dcdf94a2a8"
+    })
+  }
+}
+</script>
